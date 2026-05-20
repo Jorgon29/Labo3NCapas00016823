440e548a0b9995338e637790992c2309420a74b4
Jorgon29 <00016823@uca.edu.sv>
2026-05-20 12:29:50 -0600

    :tada:

---
+++ src/main/java/com/example/labo3ncapas00016823/Labo3NCapas00016823Application.java
@@ -0,0 +1,13 @@
+package com.example.labo3ncapas00016823;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Labo3NCapas00016823Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Labo3NCapas00016823Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/labo3ncapas00016823/Labo3NCapas00016823ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labo3ncapas00016823;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Labo3NCapas00016823ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
