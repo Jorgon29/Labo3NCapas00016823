24bfadd46e221d544f343c795871b239ddf238ee
Jorgon29 <00016823@uca.edu.sv>
2026-06-25 23:43:57 -0600

    test commit

--- src/main/java/com/example/labo3ncapas00016823/domain/dto/response/GeneralResponse.java
+++ src/main/java/com/example/labo3ncapas00016823/domain/dto/response/GeneralResponse.java
@@ -1,5 +1,6 @@
 package com.example.labo3ncapas00016823.domain.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
@@ -7,6 +8,7 @@ import java.time.LocalDateTime;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class GeneralResponse {
     private String uri;
     private String message;
